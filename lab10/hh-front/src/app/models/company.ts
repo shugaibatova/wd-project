edcfc0fc30f4fdb6a38f712c7dbd3b9ce3a2574b
shugaibatova <157125343+shugaibatova@users.noreply.github.com>
2025-04-09 16:47:41 +0600

    Delete lab10 directory

--- lab10/hh-front/src/app/models/company.ts
+++
@@ -1,9 +0,0 @@
-// src/app/models/company.ts
-export interface Company {
-    id: number;
-    name: string;
-    description: string;
-    city: string;
-    address: string;
-  }
-  
--- lab10/hh-front/src/app/models/vacancy.ts
+++
@@ -1,9 +0,0 @@
-// src/app/models/vacancy.ts
-export interface Vacancy {
-    id: number;
-    name: string;
-    description: string;
-    salary: number;
-    company: number;
-  }
-  
